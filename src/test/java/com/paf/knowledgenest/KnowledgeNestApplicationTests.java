8737324770e660808c77b081025eb09e84879e20
gimashi123 <gimashiishara2@gmail.com>
2025-03-30 22:26:04 +0530

    initial commit

---
+++ src/test/java/com/paf/knowledgenest/KnowledgeNestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paf.knowledgenest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KnowledgeNestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
