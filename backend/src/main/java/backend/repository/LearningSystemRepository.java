d7b918da4c6ed7c030949736ce0b7c32eca663bc
Yasitha Bhanuka <yasithabhanukac@gmail.com>
2025-05-01 12:28:57 +0530

    Rest the folder structure

--- backend/src/main/java/backend/repository/LearningSystemRepository.java
+++
@@ -1,7 +0,0 @@
-package backend.repository;
-
-import backend.model.LearningSystemModel;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface LearningSystemRepository extends MongoRepository<LearningSystemModel,String> {
-}
